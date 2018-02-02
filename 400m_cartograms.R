08de7693e4361a08675baee53584aa59a8792e7b
popovs <sarah.popov@gmail.com>
2018-02-02 12:52:54 -0800

    Commented out all FFTW section; make GGPLOT section clearer. Cleaned up version for Lissie.

--- 400m_cartograms.R
+++ 400m_cartograms.R
@@ -73,27 +73,33 @@ if (!require(dplyr)) {
   install.packages("dplyr", repos = "http://cran.stat.sfu.ca/")
   require(dplyr)
 }
+if (!require(maptools)) {
+  install.packages("maptools", repos = "http://cran.stat.sfu.ca/")
+  require(maptools)
+}
 if (!require(cartogram)) {
   install.packages("cartogram", repos = "http://cran.stat.sfu.ca/")
   require(cartogram)
 }
 
+# JK FFTW IS A HOT MESS, WILL CONFIDENTLY DELETE IT ONCE I GET THE REGULAR CARTOGRAM PACKAGE WORKING!!
+
 # FIRST, DOWNLOAD THE LATEST FFTW (Fast Fourier Transform) PACKAGE. The next two GitHub cartogram packages will NOT install without these binaries. http://www.fftw.org/download.html 
 
 # Also install the R package "fftw": 
-if (!require(fftw)) {
-  install.packages("fftw", repos = "http://cran.stat.sfu.ca/")
-  require(fftw) # fast Fourier transform
-}
+#if (!require(fftw)) {
+#  install.packages("fftw", repos = "http://cran.stat.sfu.ca/")
+#  require(fftw) # fast Fourier transform
+#}
 
 # Install & load devtools in order to use GitHub packages
-if (!require(devtools)) {
-  install.packages("devtools", repos = "http://cran.stat.sfu.ca/")
-  require(devtools)
-}
-install_github('omegahat/Rcartogram')
+#if (!require(devtools)) {
+#  install.packages("devtools", repos = "http://cran.stat.sfu.ca/")
+#  require(devtools)
+#}
+#install_github('omegahat/Rcartogram')
 # Wait for installation, and then:
-install_github('chrisbrunsdon/getcartr', subdir='getcartr')
+#install_github('chrisbrunsdon/getcartr', subdir='getcartr')
 
 # ----------------
 # 02 CSV READ AND DATAFRAME CREATION
@@ -106,12 +112,13 @@ c1970 <- fishing_data[fishing_data$Year==1970,]
 c1990 <- fishing_data[fishing_data$Year==1990,]
 c2014 <- fishing_data[fishing_data$Year==2014,]
 
-# In the v near future: create one large csv with the following columns: Country NAME, CATCH1950, CATCH1970, CATCH1990, CATCH2014
+# In the v near future: create one large csv with the following columns: Country NAME, CATCH1950, CATCH1970, CATCH1990, CATCH2014 to quickly recreate the fig w 4 maps
 
 # ----------------
 # 03 PREPARE GIS DATA
 # ----------------
 
+data("wrld_simpl") # Simple world dataset from maptools
 world <- wrld_simpl[wrld_simpl$NAME != "Antarctica",] # World GIS SpatialPolygonsDataFrame (spdf) from base R, minus Antarctica
 
 names(c1950) <- c("NAME", "YEAR", "CATCH") # Rename "Country" column to "NAME" so we can merge our catch data with the spdf, using the NAME column to link between the two. Capitalizing Year and Catch to make things more consistent once merged.
@@ -123,32 +130,32 @@ map1950$CATCH[is.na(map1950$CATCH)] <- 0
 # Now fill all values < 1 with 1 so cartogram actually works (otherwise countries with zeros will just be missing)
 map1950$CATCH[map1950$CATCH < 1] <- 1
 
-# Now make the cartogram! FYI this will take FOREVER. Each country takes ~30 seconds to process. There are 246 countries. 
-carto1950 <- cartogram(map1950, "CATCH", itermax=7)
+# Now make the cartogram! FYI this will take FOREVER. Each iteration takes ~30 seconds. 
+carto1950 <- cartogram(map1950, "CATCH", itermax=50)
 plot(carto1950)
-
-
-
-countries <- map_data("world")
-countries <- countries[countries$region!='Antarctica',1:5] # remove Antarctica, drop irrelevant columns
-names(countries)[names(countries) == "region"] <- "Country" # rename "region" column to "Country" so we can join the two datasets
-
-# Join map data to fishing data
-map_data_1950 <- merge(countries, c1950, by="Country", all=TRUE)
-map_data_1970 <- merge(countries, c1970, by="Country", all=TRUE)
-map_data_1990 <- merge(countries, c1990, by="Country", all=TRUE)
-map_data_2014 <- merge(countries, c2014, by="Country", all=TRUE)
-
-# Reorder the map data back into the correct order (merging messes that up)
-map_data_1950 <- arrange(map_data_1950, order)
-map_data_1970 <- arrange(map_data_1970, order)
-map_data_1990 <- arrange(map_data_1990, order)
-map_data_2014 <- arrange(map_data_2014, order)
 
 
 # ----------------
 # 04 PLOT
 # ----------------
+
+# ORIGINAL GGPLOT TESTING
+
+#countries <- map_data("world")
+#countries <- countries[countries$region!='Antarctica',1:5] # remove Antarctica, drop irrelevant columns
+#names(countries)[names(countries) == "region"] <- "Country" # rename "region" column to "Country" so we can join the two datasets
+
+# Join map data to fishing data
+#map_data_1950 <- merge(countries, c1950, by="Country", all=TRUE)
+#map_data_1970 <- merge(countries, c1970, by="Country", all=TRUE)
+#map_data_1990 <- merge(countries, c1990, by="Country", all=TRUE)
+#map_data_2014 <- merge(countries, c2014, by="Country", all=TRUE)
+
+# Reorder the map data back into the correct order (merging messes that up)
+#map_data_1950 <- arrange(map_data_1950, order)
+#map_data_1970 <- arrange(map_data_1970, order)
+#map_data_1990 <- arrange(map_data_1990, order)
+#map_data_2014 <- arrange(map_data_2014, order)
 
 p1950 <- ggplot() + 
   # countries
@@ -170,10 +177,3 @@ p1950 <- ggplot() +
             )
 
 plot(p1950)
-
-
-
-#carto1950 <- quick.carto(map_data_1950, )
-
-#cartogram(map_data_1950, "Catch", itermax=5)
-

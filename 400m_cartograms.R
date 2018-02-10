159c09442acf394923e731428b4b65ec7b040d33
popovs <sarah.popov@gmail.com>
2018-02-09 16:54:13 -0800

    Incorporated index reset & index id column in fishtogram function for later ggplot tidy merging.

--- 400m_cartograms.R
+++ 400m_cartograms.R
@@ -48,10 +48,6 @@ if (!require(maptools)) {
   install.packages("maptools", repos = "http://cran.stat.sfu.ca/")
   require(maptools)
 }
-if (!require(cartogram)) {
-  install.packages("cartogram", repos = "http://cran.stat.sfu.ca/")
-  require(cartogram)
-}
 if (!require(broom)) {
   install.packages("broom", repos = "http://cran.stat.sfu.ca/")
   require(broom)
@@ -176,12 +172,11 @@ carto_maps <- list() # Empty list that will contain cartogram output.
 
 # THIS IS A HOT MESS ON WINDOWS BUT MAKES NICE CARTOGRAMS. MUCH easier if you're working on a Unix system.
 # HOW TO GET FFTW/RCARTOGRAM TO WORK ON WINDOWS: (most recent) https://github.com/Geoff99/Rcartogram/blob/WindowsInstall/vignettes/README.WindowsInstall.Tutorial.Rmd 
-# (but also helpful) https://stackoverflow.com/questions/31613119/installing-rcartogram-packages-error-message 
+# (but also helpful, I left some comments on the answer if you ever run into any of those issues) https://stackoverflow.com/questions/31613119/installing-rcartogram-packages-error-message 
 
- # 01 FIRST, DOWNLOAD THE LATEST FFTW (Fast Fourier Transform) PACKAGE. The next two GitHub cartogram packages will NOT install without these binaries. http://www.fftw.org/download.html 
-   # HOW TO INSTALL FFTW ON WINDOWS FOR DUMMIES: https://stackoverflow.com/questions/39675436/how-to-get-fftw-working-on-windows-for-dummies
- # 02 NEXT, install RTOOLS. Rcartogram (or any other R package that relies on C, for that matter) requires Rtools to work.
-   #  
+ # 01 FIRST, DOWNLOAD THE LATEST FFTW (Fast Fourier Transform) PACKAGE (both 32 bit & 64 bit). The next two GitHub cartogram packages will NOT install without these binaries. http://www.fftw.org/download.html 
+ # 02 NEXT, install RTOOLS. Rcartogram (or any other R package that relies on C, for that matter) requires Rtools to work. https://github.com/stan-dev/rstan/wiki/Install-Rtools-for-Windows 
+ # 03. NEXT, go to the tutorial in the very first link to get this nightmare off the ground and running. Once you do, it will be so worth it. 
 
 # Also install the R package "fftw": 
 if (!require(fftw)) {
@@ -205,17 +200,16 @@ install_github('chrisbrunsdon/getcartr', subdir='getcartr')
 library(Rcartogram)
 library(getcartr)
 
-testcarto <- quick.carto(map_years[["map1956"]], map_years[["map1956"]]@data$CATCH, blur = 1)
-plot(testcarto, main="carto1956")
-
 fishtogram <- function(year) {
   print(year)
   dfname <- paste0("carto",year) # name of cartogram being made
   map_year <- get(paste0("map", year), map_years) # Create 'map_year' and fill it with one SpatialPolygonsDataFrame of a year of fishing/country data pulled from the list of spdf's 'map_years'
   carto_maps[[dfname]] <<- quick.carto(map_year, map_year@data$CATCH, blur = 1) # Create cartogram named 'dfname', chuck it into the carto_maps list
+  rownames(carto_maps[[dfname]]@data) <<- 1:nrow(carto_maps[[dfname]]@data) # Reset row names/numbers index
+  carto_maps[[dfname]]@data$id <<- seq.int(nrow(carto1950@data)) # Create ID column based on index of dataframe; this is what becomes the "id" column when you tidy the dataset for ggplot. We'll then use this id column to join the catch data from our original dataset to the tidied ggplot dataset later.
   plot(carto_maps[[dfname]], main=dfname) # plot it
   print(paste("Finished", dfname, "at", Sys.time())) # print time finished cartogram
-  writeOGR(obj = carto_maps[[dfname]], dsn = "Shapefiles", layer = dfname, driver = "ESRI Shapefile", overwrite_layer=TRUE) # Save shapefile, overwrite old ones if necessary, otherwise this forever code ABORTS when it was supposed to be running OVERNIGHT -_-
+  writeOGR(obj = carto_maps[[dfname]], dsn = "Shapefiles", layer = dfname, driver = "ESRI Shapefile", overwrite_layer=TRUE) # Save shapefile, overwrite old ones if necessary
 }
 
 # Testing 2 years
@@ -229,57 +223,98 @@ lapply(years, fishtogram)
 # 05 PLOT
 # ----------------
 
-# Transform all from SpatialPolygonsDataFrame to ggplot-friendly dataframe
-gg1950 <- tidy(carto1950) # This works
-gg1950 <- merge(gg1950, carto1950@data, by="id") # Need to add index "id" column in original carto1950 data for this merge to work
+carto1950 <- carto_maps[["carto1950"]] # Pull one map for testing
 
-p1950 <- ggplot() + 
-  geom_polygon(data = subset(gg1950, id == 103), aes(
-                #fill = id, 
-                x = long,
-                y = lat,
-                group = group))
-plot(p1950)
+ggdata <- tidy(carto1950)
+ggdata <- merge(x = ggdata, y = carto1950@data[,c("NAME", "id")], by="id", all.x=TRUE) # Join original country & catch data to tidied dataset
+#ggdata <- merge(x = ggdata, y = fishing_data[,c("NAME", "CATCH")], by="NAME", all.x = TRUE) # This is messy, but merging with original dataset bc changed 0s to 1s in the cartogram plots to get them to work. Need 0s for plotting color scale. 
+ggdata$Catch[is.na(ggdata$Catch)] <- 0 # replace NAs with 0
+ggdata <- arrange(ggdata, order) # tidy up again for fussy ggplot
 
+# Set bins
+bins <- c(0, 2, 5000, 20000, 50000, 100000, 200000, 300000, 407719) # Anything with 1 catch in the dataset is actually binned as zero bc I changed all zeros to 1s for cartogram calculation
+ggdata$bins <- cut(
+  ggdata$Catch, 
+  breaks = bins, 
+  labels = c("0", "1-5000", "5001 - 20 000", "20 001 - 50 000", "50 001 - 100 000", "100 001 - 200 000", "200 001 - 300 001", "300 001 - 407719"),
+  right = FALSE
+)
 
-# ORIGINAL GGPLOT TESTING
-
-# NEED TO GO BACK TO USING COUNTRIES DATASET 
-# Doesn't split Chukchi peninsula
-
-countries <- map_data("world")
-#countries <- countries[countries$region!='Antarctica',1:5] # remove Antarctica, drop irrelevant columns
-#names(countries)[names(countries) == "region"] <- "Country" # rename "region" column to "Country" so we can join the two datasets
-
-# Join map data to fishing data
-#map_data_1950 <- merge(countries, c1950, by="Country", all=TRUE)
-#map_data_1970 <- merge(countries, c1970, by="Country", all=TRUE)
-#map_data_1990 <- merge(countries, c1990, by="Country", all=TRUE)
-#map_data_2014 <- merge(countries, c2014, by="Country", all=TRUE)
-
-# Reorder the map data back into the correct order (merging messes that up)
-#map_data_1950 <- arrange(map_data_1950, order)
-#map_data_1970 <- arrange(map_data_1970, order)
-#map_data_1990 <- arrange(map_data_1990, order)
-#map_data_2014 <- arrange(map_data_2014, order)
-
-p1950 <- ggplot() + 
-  # countries
-  geom_polygon(data = countries, aes(
-                                  #fill = Catch,
-                                  x = long, 
-                                  y = lat,
-                                  group = group
-                                  )
-               ) +
+# Basic plot
+p1950 <- ggplot(
+  # set mappings for each layer
+  data = ggdata, 
+  aes(
+    x = long, 
+    y = lat, 
+    group = group
+  )
+) +
+  # cartogram
+  geom_polygon(
+    aes (fill = bins)
+  ) +
+  # cartogram outlines
+  geom_path(
+    color = "#5e5e5e", #2b2b2b
+    size = 0.5
+  ) +
   # constrain proportions
-  coord_fixed() +
-  # countries outlines
-  geom_path(data = countries, aes(
-                                  x = long,
-                                  y = lat,
-                                  group = group),
-            color = "white", size = 0.01
-            )
+  coord_fixed()
+#plot(p1950)
 
+# Now make the theme nice
+library(showtext)
+font_add_google("Karla", "karla") # Add nice google font
+showtext_auto() # Tell R to use showtext to render google font
+# Nice fonts don't work >:(
+
+#Manually downloaded Karla
+windowsFonts(Karla=windowsFont("Karla"))
+
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(family = "Karla", color = "#22211d"),
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      #panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+      panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+      legend.background = element_rect(fill = "#f5f5f2", color = NA),
+      panel.border = element_blank(),
+      ...
+    )
+}
+
+p1950 <- p1950 + 
+  theme_map() +
+  labs(
+    x = NULL,
+    y = NULL,
+    title = "1950",
+    caption = "FAO + SAU data"
+  )
+#plot(p1950)
+
+# Better color scale
+library(RColorBrewer)
+col.pal <- brewer.pal(7, "Spectral") # Add nice Yellow-green-blue palette for colored legend items
+#col.pal <- rev(col.pal) # reverse color order
+col.pal <- c("#b7b7b7", col.pal) # Add grey to palette for 0 catch legend items
+p1950 <- p1950 +
+  scale_fill_manual(
+    values = col.pal,
+    name = "Catch (tonnes)"
+  )
 plot(p1950)
+
+
+
